2043658e0dd1e4ef01f05742e8578a431c8fa7e1
rodyherrera <contact@rodyherrera.com>
2026-06-10 04:51:53 -0400

    feat: update coretoolkit and structure-identification dependencies to allow version ranges

--- conanfile.py
+++ conanfile.py
@@ -12,8 +12,8 @@ class OpenDXAConan(ConanFile):
     requires = (
         "boost/1.88.0",
         "onetbb/2021.12.0",
-        "coretoolkit/2.0.0",
-        "structure-identification/2.0.0",
+        "coretoolkit/[>=2.0]",
+        "structure-identification/[>=2.0]",
         "spdlog/1.14.1",
         "nlohmann_json/3.11.3",
         "yaml-cpp/0.8.0",
