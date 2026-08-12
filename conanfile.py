f806f1e0d48e77c90e8fcabbffa45a3dbab86476
rodyherrera <contact@rodyherrera.com>
2026-08-11 21:55:24 -0400

    feat: restore plugin arguments node in plugin.json; bump version in conanfile.py, plugin.json, and vpm.json

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 class OpenDXAConan(ConanFile):
     name = "opendxa"
-    version = "2.1.0"
+    version = "2.1.1"
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
